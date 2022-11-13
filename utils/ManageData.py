2da3b922e9d20dd99a0a41dd630474d455b36b32
dlams <dlalsdn1009@naver.com>
2022-11-13 11:15:47 +0900

    Update DB TESTING

--- utils/ManageData.py
+++ utils/ManageData.py
@@ -1,22 +1,28 @@
-import pymysql
-from dotenv import load_dotenv
-import os
+# import pymysql
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv()
+# load_dotenv()
+#
+# DB_USER = os.environ.get('DB_USER')
+# DB_PW = os.environ.get('DB_PW')
+#
+# conn = pymysql.connect(host='127.0.0.1', user=DB_USER, password=DB_PW, db='PURUN', charset='utf8')
+# sql = conn.cursor()
+#
+# def SelectData(query):
+#     if query == "":
+#         sql.execute(f"SELECT * FROM PURUN_TB")
+#     else:
+#         sql.execute(f"SELECT * FROM PURUN_TB WHERE {query}")
+#     return sql.fetchall()
+#
+# def InsertData(data):
+#     sql.execute(f"INSERT INTO PURUN_TB (STAMP, TEMP, PH, WATER_LEV, LIGHT_LEV, LAST_FEED, PLANT_HEIGHT, PLANT_WIDTH) VALUES ({data})")
+#     conn.commit()
 
-DB_USER = os.environ.get('DB_USER')
-DB_PW = os.environ.get('DB_PW')
-
-conn = pymysql.connect(host='127.0.0.1', user=DB_USER, password=DB_PW, db='PURUN', charset='utf8')
-sql = conn.cursor()
 
 def SelectData(query):
-    if query == "":
-        sql.execute(f"SELECT * FROM PURUN_TB")
-    else:
-        sql.execute(f"SELECT * FROM PURUN_TB WHERE {query}")
-    return sql.fetchall()
-
+    return f"get {query}"
 def InsertData(data):
-    sql.execute(f"INSERT INTO PURUN_TB (STAMP, TEMP, PH, WATER_LEV, LIGHT_LEV, LAST_FEED, PLANT_HEIGHT, PLANT_WIDTH) VALUES ({data})")
-    conn.commit()
+    return f"insert {data}"
